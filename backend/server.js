0d1dc4e89785a24c65457e3265944b73e8dcc858
Nilav Prajapati <nilavprajapati2004@gmail.com>
2024-03-03 18:25:49 +0530

    project initiated

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+// const blockRouter = require("./routes/blockRouter");
+
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connection successful!"));
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+//example of a route
+// app.use("/api/blocks", blockRouter);
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`listening to the port ${port}`);
+});
