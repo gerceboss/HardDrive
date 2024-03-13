2b9314db6a15f93a5f7dc1f09751c0ae749c5ac5
preeti <karnwalpreeti102@gmail.com>
2024-03-13 14:24:27 +0530

    added bg image

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -6,6 +6,6 @@ router.post("/createUser", userController.createUser);
 router.get("/:addr", userController.getUser);
 router.get("/:addr/myFiles", userController.getAllFiles);
 router.get("/:addr/folders", userController.getAllFolders);
-router.post("/:addr/uploadFile", userController.uploadFile); // consider the the file and its content come in req.body and decide its format
+// router.post("/:addr/uploadFile", userController.uploadFile); // consider the the file and its content come in req.body and decide its format
 
 module.exports = router;
