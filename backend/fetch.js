476048c88eddae45ef0f18a3245e23c0fcaf462d
Nilav Prajapati <nilavprajapati2004@gmail.com>
2024-03-14 15:13:45 +0530

    added api keys

--- backend/fetch.js
+++ backend/fetch.js
@@ -3,10 +3,10 @@ const ABI = require("./ABI.json");
 const { Web3 } = require("web3");
 const web3 = new Web3(
   new Web3.providers.HttpProvider(
-    "https://little-black-ensemble.ethereum-sepolia.discover.quiknode.pro/0cf0508350900bf159739cf58bdd6846952a7a42/"
+    "https://polygon-mumbai.g.alchemy.com/v2/In2MkKQ-tdNRRqw8YteHoQtMQF41vLWi"
   )
 );
 
-const contractAddress = "0x874806B13dA2e975556CA084e52416c252CedF7F"; //change the address and the provider
+const contractAddress = "0xF4b43bdacC4023d5e0a7E0d830844f45161AD513"; //deployed on polygon mumbai
 const contract = new web3.eth.Contract(ABI, contractAddress);
 module.exports = contract;
