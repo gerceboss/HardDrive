db303345616a7de21eb2efcfc2c5dc9f3aa5588e
Sanatan Sharma <sanatan0660@gmail.com>
2024-04-09 21:49:54 +0530

    frontend-update-3

--- front-end/components/Sidebar.tsx
+++ front-end/components/Sidebar.tsx
@@ -1,22 +1,57 @@
 import React from "react";
+import { Title } from "./Title";
+import { useRouter } from "next/router";
 
-export const Sidebar = () => {
-  return (
-    <div className="sidebar">
-      <ul>
-        <li>
-          <a href="#">All Files</a>
-        </li>
-        <li>
-          <a href="#">All Folders</a>
-        </li>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/drive">Drive</a>
-        </li>
-      </ul>
-    </div>
-  );
+
+export const Sidebar = (props) =>{
+  const router = useRouter();
+
+  const goToDrivePage = () => {
+    router.push('/drive');
+  };
+  const goToHomePage = () => {
+    router.push('/');
+  };
+  const clickFolders = () =>{
+    props.setdisplayFolders(true);
+    props.setdisplayFiles(false);
+  }
+  const clickFiles = () =>{
+    props.setdisplayFolders(false);
+    props.setdisplayFiles(true);
+  }
+  const clickAll = () =>{
+    props.setdisplayFolders(true);
+    props.setdisplayFiles(true);
+  }
+
+
+    return (
+    <>
+      <div className="sidebar">
+        <ul>
+          <li>
+          <Title />
+          </li>
+          <li onClick={clickAll}>
+            <a>All Files and Folders</a>
+          </li>
+          <li onClick={clickFiles}>
+            <a>All Files</a>
+          </li>
+          <li onClick={clickFolders}>
+            <a>All Folders</a>
+          </li>
+          <li onClick={goToHomePage}>
+            <a>Home</a>
+          </li>
+          <li onClick={goToDrivePage}>
+            <a>Drive</a>
+          </li>
+        </ul>
+      </div>
+    </>
+    );
+
 };
+
